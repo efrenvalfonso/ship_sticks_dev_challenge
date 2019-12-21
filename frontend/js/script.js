571ea1d8d9b74073ff62c4427b74ae88a4adb12e
Efren Vila Alfonso <efrenvalfonso@gmail.com>
2019-12-20 22:33:08 -0500

    Adding validation to calculator fields

--- frontend/js/script.js
+++ frontend/js/script.js
@@ -4,48 +4,84 @@ function apiUrl(request) {
     return `http://localhost:3000/api/v1/${request}`;
 }
 
+function checkErrors() {
+    if ($("#calculator input.is-invalid").length > 0) {
+        $("#calculatorError").slideDown();
+        $("#calculateBtn").attr("disabled", "disabled");
+    } else {
+        $("#calculatorError").slideUp();
+        $("#calculateBtn").removeAttr("disabled");
+    }
+}
+
 $(document).ready(() => {
     let interval;
     let calculator = $('#calculator');
+
+    $("#calculator input").change((e) => {
+        let input = $(e.target);
+
+        if (isNaN(input.val()) || input.val().trim().length === 0) {
+            input.addClass("is-invalid");
+        } else {
+            input.removeClass("is-invalid");
+        }
+
+        checkErrors();
+    });
 
     $("#launchCalculatorBtn").click(() => {
         $("#result").fadeOut();
     });
 
     $("#calculateBtn").click(() => {
-        $.getJSON(apiUrl("products/find-best-fit"),
-            {
-                length: $("#calculatorLengthInput").val(),
-                width: $("#calculatorWidthInput").val(),
-                height: $("#calculatorHeightInput").val(),
-                weight: $("#calculatorWeightInput").val()
-            },
-            function (data) {
-                let calculateBtn = $("#calculateBtn");
-                let countdown = closingTimeSeconds;
+        $("#calculator input").each((index, value) => {
+            let input = $(value);
 
-                interval = setInterval(() => {
-                    if (countdown > 0) {
-                        $("#calculateBtn").text(`Closing in ${countdown--}s`);
-                    } else {
-                        clearInterval(interval);
-                        $("#calculator").modal('hide');
-                    }
-                }, 1000);
+            if (isNaN(input.val()) || input.val().trim().length === 0) {
+                input.addClass("is-invalid");
+            } else {
+                input.removeClass("is-invalid");
+            }
 
-                let productName = `${data.product_type.name} - ${data.name} (${data.length}"x${data.width}"x${data.height}"@${data.weight}lbs)`;
+            checkErrors();
+        });
 
-                $("#productNameModal").text(productName);
-                $("#productName").text(productName);
-                $("#length").text($("#calculatorLengthInput").val() || 0);
-                $("#width").text($("#calculatorWidthInput").val() || 0);
-                $("#height").text($("#calculatorHeightInput").val() || 0);
-                $("#weight").text($("#calculatorWeightInput").val() || 0);
-                $("#resultModal").slideDown();
+        if ($("#calculator input.is-invalid").length === 0) {
+            $.getJSON(apiUrl("products/find-best-fit"),
+                {
+                    length: $("#calculatorLengthInput").val(),
+                    width: $("#calculatorWidthInput").val(),
+                    height: $("#calculatorHeightInput").val(),
+                    weight: $("#calculatorWeightInput").val()
+                },
+                function (data) {
+                    let calculateBtn = $("#calculateBtn");
+                    let countdown = closingTimeSeconds;
 
-                calculateBtn.attr("disabled", "disabled");
-                calculateBtn.text(`Closing in ${countdown--}s`);
-         });
+                    interval = setInterval(() => {
+                        if (countdown > 0) {
+                            $("#calculateBtn").text(`Closing in ${countdown--}s`);
+                        } else {
+                            clearInterval(interval);
+                            $("#calculator").modal('hide');
+                        }
+                    }, 1000);
+
+                    let productName = `${data.product_type.name} - ${data.name} (${data.length}"x${data.width}"x${data.height}"@${data.weight}lbs)`;
+
+                    $("#productNameModal").text(productName);
+                    $("#productName").text(productName);
+                    $("#length").text($("#calculatorLengthInput").val() || 0);
+                    $("#width").text($("#calculatorWidthInput").val() || 0);
+                    $("#height").text($("#calculatorHeightInput").val() || 0);
+                    $("#weight").text($("#calculatorWeightInput").val() || 0);
+                    $("#resultModal").slideDown();
+
+                    calculateBtn.attr("disabled", "disabled");
+                    calculateBtn.text(`Closing in ${countdown--}s`);
+                });
+        }
     });
 
     calculator.on('hidden.bs.modal', () => {
